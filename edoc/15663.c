37c7e5a91336aa4517b3e610a0ff02c0b164d4c7
hayeong0206 <dbsgkdudchlr@ewhain.net>
2021-01-20 06:00:23 +0900

    n과 m(9) 성공

--- edoc/15663.c
+++ edoc/15663.c
@@ -6,59 +6,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int N,M,i;
-int arr[9]={0};
-int check[9] = {0};
-int ans[9][9] = {0};
+int N,M;
+int arr[8];
+int ans[10];
+int check[10];
 
-void solve(int num){
-    printf("%d",num);
-    if (num >= M){
-        printf("----num == M-----\n");
-        for (i=0; i<M; i++) {
-            printf("%d ",arr[i]);
-        }
-        printf("\n");
-        return;
-    }
-
-    for(i=0; i<N; i++){
-        if(check[i] == 1) continue; //같은 수를 중복하여 이용할 수 없게함
-        check[i]= 1;
-        arr[num]=arr[i];
-        printf("arr[%d]: %d\n",num,arr[num]);
-        solve(num+1);
-        check[i]= 0;
-    }
-}
-
+//qsort 를 사용하기 위한 compare함수 선언 
 int compare(const void* a, const void* b){
     if(*(int*)a>*(int*)b) return 1;
     else if(*(int*)a<*(int*)b) return -1;
     else return  0;
 }
+ 
+int function(int num,int arr[8]){
+    int i;
+    //m개를 선택했다면 출력한다. 
+    if (num==M){
+        for (i=0; i<M; i++) {
+            printf("%d ",ans[i]);
+        }
+        printf("\n");
+        return num;
+    }
+    //prev_num이라는 변수를 변수를 저장해 직전에 골랐던 수를 저장해 줌.
+    int prev_num=-1;
+
+    for (i=0; i<N; i++){
+        //직전에 골랐던 수이거나 이미 고른 수라면 넘어감.
+        if(check[i]==1||prev_num==arr[i]) continue;
+        check[i] = 1; //그 수를 골라줌 
+        ans[num] = arr[i]; //ans 배열에 넣어줌 
+        prev_num = arr[i]; // 직전에 고른 수를 저장해준다.
+        function(num+1,arr); //다음 수를 고른다.
+        check[i] = 0;
+    }
+}
+    
 
 int main(){
-
     scanf("%d %d",&N, &M);
-
     for (int i=0; i<N; i++){
         scanf("%d",&arr[i]);
-    } //N개의 수 입력
-
+    }
+    
+    //사전순으로 출력하기 위해 오름차순 정렬
     qsort(arr,N,sizeof(int),compare);
-
-    printf("qsort 정렬후 \n");
-    for(i=0; i<N;i++){
-        printf("arr[%d]: %d \n",i, arr[i]);
-    }
-    printf("\n");
-
-    solve(0);
-
-    printf("재귀함수 돌린 후 배열 안에 들어간 값.\n");
-    for(i=0; i<N;i++){
-        printf("arr[%d]: %d \n",i, arr[i]);
-    }
-
+    function(0,arr);
 }
