9186bf9d4bc38aa0a0ef3318dce7ca57c6bd83ff
hayeong0206 <dbsgkdudchlr@ewhain.net>
2021-01-20 03:45:37 +0900

    n과 m(3) 성공

---
+++ 15651.c
@@ -0,0 +1,31 @@
+//n과 m(3)
+//1에서 추가된 점 - 같은 수 여러번 고르기 가능. 
+
+#include <stdio.h>
+
+int N,M;
+int ans[10];
+int check[10];
+
+int function(int num){
+    int i;
+    if (num==M){
+        
+        for (i=0; i<M; i++) {
+            printf("%d ",ans[i]);
+        }
+        printf("\n");
+        return num;
+    }
+
+    for (i=1; i<=N; i++){
+        ans[num] = i;
+        function(num+1);
+    }
+}
+    
+
+int main(){
+    scanf("%d %d",&N, &M);
+    function(0);
+}
