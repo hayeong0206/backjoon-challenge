70db232c6979cc6c0c8ce2146ca61daa9658fbee
hayeong0206 <dbsgkdudchlr@ewhain.net>
2021-02-02 00:25:26 +0900

    게으른 백곰 주석 추가

--- edoc/10025.c
+++ edoc/10025.c
@@ -18,10 +18,14 @@ int main(){
     }
 
     k = k*2 +1;
+    // k*2+1 범위 내에서 최댓값 찾는 문제
     for(i = 0; i<=1000000; i++){
         if(i>= k) sum -= ice[i-k];
+        //i가 범위를 넘어간다면 더해준 값 중에서 범위 밖에 있는 값을 빼주기
         sum += ice[i];
+        //범위 안에 있는 값들 더해주기 
         ans = sum > ans? sum:ans;
+        // 큰 값 정해주기
     }
     printf("%d\n",ans);
     return 0;
