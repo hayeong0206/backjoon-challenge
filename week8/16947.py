eeefec176250cd0cc11b435ad25b6144bfd5f6d7
hayeong0206 <dbsgkdudchlr@ewhain.net>
2025-05-18 00:00:58 +0900

    week8

---
+++ week8/16947.py
@@ -0,0 +1,73 @@
+'''
+서울 지하철 2호선
+
+입력:역의 개수, 역과 역을 연결하는 구간 정보
+출력: 역과 순환선과의 거리 N개의 정수
+'''
+
+from collections import deque
+import sys
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+
+n = int(input())
+
+graph = [[] for _ in range(n+1)]
+
+cycle = set()
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [False] * (n + 1)
+
+cy = False
+
+def dfs(start, here, level, li):
+    global cy
+    if level <= 2:
+        for v in graph[here]:
+            if visited[v] == False:
+                visited[v] = True
+                dfs(start, v, level+1, li+[v])
+                visited[v] = False
+    else:
+        for v in graph[here]:
+            if visited[v] == False:
+                visited[v] = True
+                dfs(start, v, level+1, li+[v])
+                visited[v] = False
+            else:
+                if v == start:
+                    cy = True
+                    for l in li:
+                        cycle.add(l)
+                    return
+
+for i in range(1, n+1):
+    if cy == True:
+        break
+    visited[i] = True
+    dfs(i, i, 1, [i])
+    visited[i] = False
+
+cycle = list(cycle)
+# bfs 수행
+answer = [int(1e9)] * (n + 1)
+q = deque()
+for c in cycle:
+    q.append((c, 0))
+    answer[c] = 0
+
+while q:
+    node, level = q.popleft()
+
+    for v in graph[node]:
+        if answer[v] == int(1e9):
+            answer[v] = level+1
+            q.append((v, level+1))
+
+
+print(" ".join(map(str, answer[1:])))
